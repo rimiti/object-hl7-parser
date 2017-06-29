73c493ed0e594a068fd62527efa7536463c1a1da
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-06-29 14:36:14 +0200

    chore(test/*.js) test template created

---
+++ test/adt.js
@@ -0,0 +1,17 @@
+import parser from '../lib/parser'
+import chai from 'chai'
+chai.should()
+
+describe('ADT', () => {
+  it(`A04`, (done) => {
+    done()
+  })
+
+  it(`A08`, (done) => {
+    done()
+  })
+
+  it(`A40`, (done) => {
+    done()
+  })
+})
---
+++ test/siu.js
@@ -0,0 +1,30 @@
+import parser from '../lib/parser'
+import chai from 'chai'
+chai.should()
+
+describe('SIU', () => {
+
+  it(`S12`, (done) => {
+    done()
+  })
+
+  it(`S13`, (done) => {
+    done()
+  })
+
+  it(`S14`, (done) => {
+    done()
+  })
+
+  it(`S15`, (done) => {
+    done()
+  })
+
+  it(`S17`, (done) => {
+    done()
+  })
+
+  it(`S26`, (done) => {
+    done()
+  })
+})
