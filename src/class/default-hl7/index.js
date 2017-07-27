cacb89448ab2772d34da46f96bbb598baee5d34c
Dimitri <contact@dimsolution.com>
2017-07-27 11:28:57 +0200

    refact(class/default-hl7/index.js) import removed because not used

--- src/class/default-hl7/index.js
+++ src/class/default-hl7/index.js
@@ -1,11 +1,7 @@
-import simpleHL7 from 'simple-hl7'
 import ADT from './hl7/adt'
 import SIU from './hl7/siu'
 
 export default class DefaultEncoder {
-
-  constructor() {
-  }
 
   getAdtEncoder(message) {
     return new ADT(message)
