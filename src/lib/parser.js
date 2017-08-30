f76cc4aa86afdf683e0b099ef3c3a66a107ad68a
Dimitri <contact@dimsolution.com>
2017-08-30 17:34:42 +0200

    feat(lib/parser.js) wrapper compliant with babel 6.x

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,8 +1,45 @@
-import parser from '../class'
+import DefaultEncoder from '../class/default-hl7'
 
-parser.configure({
-    "version": "hl7-2.4"
-  }
-)
+const defaultConfiguration = {"version": "hl7-2.4"}
+let encoder = {}
 
-export default parser
+export function configure(config = defaultConfiguration) {
+  if (config.version === 'hl7-2.4') return encoder = new DefaultEncoder()
+  throw new Error(`Unknown format: ${config.version}`)
+}
+
+export function getADT04(message, config) {
+  return encoder.getAdtEncoder(message).getA04(config)
+}
+
+export function getADT08(message, config) {
+  return encoder.getAdtEncoder(message).getA08(config)
+}
+
+export function getADT40(message, config) {
+  return encoder.getAdtEncoder(message).getA40(config)
+}
+
+export function getSIU12(message, config) {
+  return encoder.getSiuEncoder(message).getS12(config)
+}
+
+export function getSIU13(message, config) {
+  return encoder.getSiuEncoder(message).getS13(config)
+}
+
+export function getSIU14(message, config) {
+  return encoder.getSiuEncoder(message).getS14(config)
+}
+
+export function getSIU15(message, config) {
+  return encoder.getSiuEncoder(message).getS15(config)
+}
+
+export function getSIU17(message, config) {
+  return encoder.getSiuEncoder(message).getS17(config)
+}
+
+export function getSIU26(message, config) {
+  return encoder.getSiuEncoder(message).getS26(config)
+}
